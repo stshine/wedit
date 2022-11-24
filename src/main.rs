0d23b3e8754d94ce6fd6e20e5df68e548581afb8
Pu Xingyu <pu.stshine@gmail.com>
2022-11-24 14:21:38 +0800

    Add crates for text rendering

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+extern crate app_units;
+extern crate euclid;
+extern crate gleam;
+extern crate glutin;
+extern crate rustybuzz;
+extern crate webrender;
+extern crate webrender_api;
+extern crate winit;
+
 fn main() {
     println!("Hello, world!");
 }
